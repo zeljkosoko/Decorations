1aa57441e3b8ecda5195a26637fad819ddcd1112
Zeljkos <zeljkos@ever-near.com>
2019-11-05 16:41:00 +0100

    Adding element to shopping list with localReferencing with ViewChild and event binding

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Element } from "../../shared/element.model";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  //local references with @ViewChild
+  @ViewChild('inputName') inputNameReference: ElementRef;
+  @ViewChild('inputAmount') inputAmountReference: ElementRef;
+
+  @Output() elementAdded: EventEmitter<Element> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddElement() {
+    let elementName = this.inputNameReference.nativeElement.value;
+    let elementAmount = this.inputAmountReference.nativeElement.value;
+    let el = new Element(elementName,elementAmount);
+
+    this.elementAdded.emit(el);
+  }
 }
